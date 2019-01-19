fbe8c9f7397035e5bb7c01f969e0a4acc407f93e
A-Game <javascript@adamheldring.com>
2019-01-19 11:18:55 +0100

    wip-added basic grid layout

--- code/src/components/app.js
+++ code/src/components/app.js
@@ -1,8 +1,39 @@
 import React from "react";
 
 class App extends React.Component {
+
+state = {
+  windowHeight: 0,
+  windowWidth: 0
+}
+
+componentDidMount() {
+  window.addEventListener("resize", this.updateWindowSize)
+  this.updateWindowSize()
+}
+
+updateWindowSize = () => {
+  this.setState({
+    vpWidth: window.innerWidth,
+    vpHeight: window.innerHeight
+  })
+}
+
   render() {
-    return <div>RPG GRID</div>;
+    const { vpWidth, vpHeight } = this.state
+
+    const boxList = new Array(50).fill("")
+
+    console.log(boxList)
+    return (
+      <div className="mainWrapper" style={{ width: vpWidth-2, height: vpHeight-2 }}>
+        <div className="grid">
+          {boxList.map(box => {
+            return <div className="grid__box"></div>
+          })}
+        </div>
+      </div>
+    );
   }
 }
 
