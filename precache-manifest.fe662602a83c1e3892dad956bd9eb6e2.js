37798fae720b79b4c88b08402b619ddc2a3258cf
A-Game <javascript@adamheldring.com>
2019-01-21 22:58:34 +0100

    Updates

---
+++ precache-manifest.fe662602a83c1e3892dad956bd9eb6e2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "af9d332c1f75020558570419602e0d8e",
+    "url": "/RpgGrid/static/media/grass-web.af9d332c.jpg"
+  },
+  {
+    "revision": "8c6043efcc4370c8e2b3",
+    "url": "/RpgGrid/static/js/runtime~main.8c6043ef.js"
+  },
+  {
+    "revision": "f7b744a385273c8a6375",
+    "url": "/RpgGrid/static/js/main.f7b744a3.chunk.js"
+  },
+  {
+    "revision": "08fc626d026ca21455fd",
+    "url": "/RpgGrid/static/js/1.08fc626d.chunk.js"
+  },
+  {
+    "revision": "f7b744a385273c8a6375",
+    "url": "/RpgGrid/static/css/main.0781dec4.chunk.css"
+  },
+  {
+    "revision": "f31738f47e695faacede1f3fab537f88",
+    "url": "/RpgGrid/index.html"
+  }
+];
